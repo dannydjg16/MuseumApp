dc7b0fa115287bf5c5ee86631e5a3edfc30d68f1
dannydjg16 <dannydjg16@gmail.com>
2022-02-15 12:42:37 -0500

    Added User Interface

---
+++ MuseumApp.Domain/Interfaces/IUserInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MuseumApp.Domain.Models;
+
+namespace MuseumApp.Domain.Interfaces
+{
+    public interface IUserInterface
+    {
+        bool CreateAccount(User user);
+        bool DeleteAccount(int userID);
+        bool EditAccount(User user);
+
+        IEnumerable<User> GetUsers(string name = null);
+        User GetUserByID(int id);
+        User GetUserByEmail(string email);
+    }
+}
