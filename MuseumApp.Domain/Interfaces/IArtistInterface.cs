d7654a403a5aabf75d194461f10d93cacacf7a78
dannydjg16 <dannydjg16@gmail.com>
2022-02-21 15:36:27 -0500

    Artist Domain Interface

---
+++ MuseumApp.Domain/Interfaces/IArtistInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using MuseumApp.Domain.Models;
+
+namespace MuseumApp.Domain.Interfaces
+{
+    public interface IArtistInterface
+    {
+        bool CreateArtist(Artist artist);
+        bool DeleteArtist(int artistID);
+        bool EditArtist(Artist artist);
+        IEnumerable<Artist> GetArtists(string artistName = null);
+        Artist GetArtistByID(int id);
+    }
+}
