3e52ab341baf07b9c58540e5ea63804ad102c41b
dannydjg16 <dannydjg16@gmail.com>
2022-02-23 00:40:11 -0500

    ArtistModelMapper completed

--- MuseumApp.WebAPI/Mappers/ArtistModelMapper.cs
+++ MuseumApp.WebAPI/Mappers/ArtistModelMapper.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using MuseumApp.WebAPI.Models;
+
 namespace MuseumApp.WebAPI.Mappers
 {
-    public class ArtistModelMapper
+    public static class ArtistModelMapper
     {
-        public ArtistModelMapper()
+        public static ArtistModel Map(Domain.Models.Artist artist)
         {
+            return new ArtistModel
+            {
+                ID = artist.ID,
+                Biography = artist.Biography,
+                Born = artist.Born,
+                Died = artist.Died,
+                BornLocation = artist.BornLocation,
+                Name = artist.Name,
+                PictureURL = artist.PictureURL
+            };
+        }
+
+        public static Domain.Models.Artist Map(ArtistModel model)
+        {
+            return new Domain.Models.Artist
+            {
+                ID = model.ID,
+                Biography = model.Biography,
+                Born = model.Born,
+                Died = model.Died,
+                BornLocation = model.BornLocation,
+                Name = model.Name,
+                PictureURL = model.PictureURL
+            };
         }
     }
 }
