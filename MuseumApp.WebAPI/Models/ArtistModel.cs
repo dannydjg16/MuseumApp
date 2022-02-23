f32d3b4f636aa25b34de85de99dff1e491c23464
dannydjg16 <dannydjg16@gmail.com>
2022-02-23 00:28:11 -0500

    Artwork model added for the artists artworks. Artworks I added all fields

--- MuseumApp.WebAPI/Models/ArtistModel.cs
+++ MuseumApp.WebAPI/Models/ArtistModel.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace MuseumApp.WebAPI.Models
 {
     public class ArtistModel
     {
-        public ArtistModel()
-        {
-        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string BornLocation { get; set; }
+        public string Biography { get; set; }
+        public string PictureURL { get; set; }
+        public string Born { get; set; }
+        public string Died { get; set; }
+        public IEnumerable<ArtworkModel> Artworks { get; set; }
     }
 }
---
+++ MuseumApp.WebAPI/Models/ArtworkModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MuseumApp.WebAPI.Models
+{
+    public class ArtworkModel
+    {
+        public ArtworkModel()
+        {
+        }
+    }
+}
