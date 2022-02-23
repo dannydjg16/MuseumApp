200de6f7e7ad18b9499136eefc4119f2041200dd
dannydjg16 <dannydjg16@gmail.com>
2022-02-23 13:09:15 -0500

    Adding fields for ArtType class
    Added the ID, Name, and Description. Planning on doing all of the models now.

--- MuseumApp.Domain/Models/ArtType.cs
+++ MuseumApp.Domain/Models/ArtType.cs
@@ -3,8 +3,8 @@ namespace MuseumApp.Domain.Models
 {
     public class ArtType
     {
-        public ArtType()
-        {
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
