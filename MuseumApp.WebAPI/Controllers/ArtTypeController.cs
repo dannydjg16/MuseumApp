5556c45b52f6e903bf0cc0f6716760f4b0fc967f
dannydjg16 <dannydjg16@gmail.com>
2022-02-24 23:33:50 -0500

    ArtType Controller done

---
+++ MuseumApp.WebAPI/Controllers/ArtTypeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MuseumApp.Domain.Interfaces;
+using MuseumApp.WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MuseumApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ArtTypeController : ControllerBase
+    {
+        private readonly IArtTypeInterface _artTypeRespository;
+
+        public ArtTypeController(IArtTypeInterface artTypeRepository)
+        {
+            _artTypeRespository = artTypeRepository;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ArtTypeModel>>> Get()
+        {
+            var artTypes = await Task.FromResult(_artTypeRespository.GetArtTypes());
+
+            if (artTypes.Select(Mappers.ArtTypeModelMapper.Map) is IEnumerable<ArtTypeModel> retrievedATMs)
+            {
+                return Ok(retrievedATMs);
+            }
+
+            return NotFound();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post(ArtTypeModel artTypeModel) 
+        {
+            var complete = await Task.FromResult(_artTypeRespository.AddArtType(Mappers.ArtTypeModelMapper.Map(artTypeModel)));
+
+            if (complete)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
