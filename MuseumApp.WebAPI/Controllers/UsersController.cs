e9d2f5e786dfb08bc9860c8bfadab04fbb355f9b
dannydjg16 <dannydjg16@gmail.com>
2022-04-06 17:31:33 -0400

    Clean Up Usings

--- MuseumApp.WebAPI/Controllers/UsersController.cs
+++ MuseumApp.WebAPI/Controllers/UsersController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MuseumApp.Domain.Interfaces;
 using MuseumApp.Domain.Models;
 using MuseumApp.WebAPI.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
