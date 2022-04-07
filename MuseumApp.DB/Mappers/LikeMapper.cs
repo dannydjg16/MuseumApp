a1e6cd522b63c50284ad89b1e80f474181ea65e1
dannydjg16 <dannydjg16@gmail.com>
2022-04-06 22:29:36 -0400

    Adding Domain Like
    Also adding the mapper from domain like to db like + vice versa

---
+++ MuseumApp.DB/Mappers/LikeMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MuseumApp.DB.Mappers
+{
+    public static class LikeMapper
+    {
+        public static Like Map(Domain.Models.Like like)
+        {
+            return new Like
+            {
+                ArtId = like.ArtId,
+                UserId = like.UserId
+            };
+        }
+
+        public static Domain.Models.Like Map(Like entity)
+        {
+            return new Domain.Models.Like
+            {
+                ArtId = entity.ArtId,
+                UserId = entity.UserId
+            };
+        }
+    }
+}
---
+++ MuseumApp.Domain/Models/Like.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MuseumApp.Domain.Models
+{
+    public class Like
+    {
+        public int UserId { get; set; }
+        public int ArtId { get; set; }
+    }
+}
