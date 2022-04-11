8f31e49ce892e1d6ec1981a1854559876cd6c687
dannydjg16 <dannydjg16@gmail.com>
2022-04-11 14:34:50 -0400

    Get Users Likes Done

---
+++ MuseumApp.WebAPI/Controllers/LikesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MuseumApp.Domain.Interfaces;
+using MuseumApp.WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MuseumApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class LikesController : ControllerBase
+    {
+        private readonly ILikesInterface _likesRepository;
+
+        public LikesController(ILikesInterface likesRepository)
+        {
+            _likesRepository = likesRepository;
+        }
+
+        // GET api/likes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<ArtworkModel>>> Get(int id)
+        {
+            var result = await Task.FromResult(_likesRepository.GetUsersLikes(id));
+
+            if (result.Select(Mappers.ArtworkModelMapper.Map) is IEnumerable<ArtworkModel> artworkModels)
+            {
+                return Ok(artworkModels);
+            }
+            return NotFound();
+        }
+
+        // POST api/likes
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/likes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
