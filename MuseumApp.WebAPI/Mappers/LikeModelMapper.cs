3e4b157a37381f61c67cf885bde9f9d4b7db8d5a
dannydjg16 <dannydjg16@gmail.com>
2022-04-11 14:14:57 -0400

    Like Model and Like Model Mapper

---
+++ MuseumApp.WebAPI/Mappers/LikeModelMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MuseumApp.WebAPI.Models;
+
+namespace MuseumApp.WebAPI.Mappers
+{
+    public static class LikeModelMapper
+    {
+        public static LikeModel Map(Domain.Models.Like like)
+        {
+            return new LikeModel
+            {
+                ArtId = like.ArtId,
+                UserId = like.UserId
+            };
+        }
+
+        public static Domain.Models.Like Map(LikeModel model)
+        {
+            return new Domain.Models.Like
+            {
+                ArtId = model.ArtId,
+                UserId = model.UserId
+            };
+        }
+    }
+}
---
+++ MuseumApp.WebAPI/Models/LikeModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MuseumApp.WebAPI.Models
+{
+    public class LikeModel
+    {
+        public int UserId { get; set; }
+        public int ArtId { get; set; }
+    }
+}
