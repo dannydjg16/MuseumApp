fbe9d4d99211072fb8f97bf5a41793069ced5317
dannydjg16 <dannydjg16@gmail.com>
2022-05-05 23:49:01 -0400

    Born Died for artist domain now int

--- MuseumApp.Domain/Models/Artist.cs
+++ MuseumApp.Domain/Models/Artist.cs
@@ -9,8 +9,8 @@ namespace MuseumApp.Domain.Models
         public string BornLocation { get; set; }
         public string Biography { get; set; }
         public string PictureURL { get; set; }
-        public string Born { get; set; }
-        public string Died { get; set; }
+        public int Born { get; set; }
+        public int Died { get; set; }
         //public IEnumerable<Artwork> Artworks { get; set; }
     }
 }
