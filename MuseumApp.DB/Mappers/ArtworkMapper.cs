7d2c06ba14f583b99fc1536851e89380eb837757
dannydjg16 <dannydjg16@gmail.com>
2022-06-27 15:45:33 -0400

    Artwork no Artist Mappers

--- MuseumApp.DB/Mappers/ArtworkMapper.cs
+++ MuseumApp.DB/Mappers/ArtworkMapper.cs
@@ -37,5 +37,39 @@
                 Artist = ArtistMapper.Map(entity.Artist)
             };
         }
+
+        public static Artwork MapNoArtist(Domain.Models.Artwork artwork)
+        {
+            return new Artwork
+            {
+                Id = artwork.Id,
+                ArtistId = artwork.ArtistId,
+                Description = artwork.Description,
+                FileName = artwork.FileName,
+                Likes = artwork.Likes,
+                LocationNow = artwork.LocationNow,
+                MediumId = artwork.MediumId,
+                Title = artwork.Title,
+                YearCreated = artwork.YearCreated,
+                ArtWorkAdderId = artwork.ArtWorkAdderId
+            };
+        }
+
+        public static Domain.Models.Artwork MapNoArtist(Artwork entity)
+        {
+            return new Domain.Models.Artwork
+            {
+                Id = entity.Id,
+                ArtistId = entity.ArtistId,
+                Description = entity.Description,
+                FileName = entity.FileName,
+                Likes = entity.Likes,
+                LocationNow = entity.LocationNow,
+                MediumId = entity.MediumId,
+                Title = entity.Title,
+                YearCreated = entity.YearCreated,
+                ArtWorkAdderId = entity.ArtWorkAdderId
+            };
+        }
     }
 }
--- MuseumApp.WebAPI/Mappers/ArtworkModelMapper.cs
+++ MuseumApp.WebAPI/Mappers/ArtworkModelMapper.cs
@@ -39,5 +39,39 @@ namespace MuseumApp.WebAPI.Mappers
                 Artist = ArtistModelMapper.Map(model.Artist)
             };
         }
+
+        public static ArtworkModel MapNoArtist(Domain.Models.Artwork artwork)
+        {
+            return new ArtworkModel
+            {
+                Id = artwork.Id,
+                Title = artwork.Title,
+                ArtistId = artwork.ArtistId,
+                Description = artwork.Description,
+                FileName = artwork.FileName,
+                Likes = artwork.Likes,
+                LocationNow = artwork.LocationNow,
+                MediumId = artwork.MediumId,
+                YearCreated = artwork.YearCreated,
+                ArtWorkAdderId = artwork.ArtWorkAdderId
+            };
+        }
+
+        public static Domain.Models.Artwork MapNoArtist(ArtworkModel model)
+        {
+            return new Domain.Models.Artwork
+            {
+                Id = model.Id,
+                Title = model.Title,
+                ArtistId = model.ArtistId,
+                Description = model.Description,
+                FileName = model.FileName,
+                Likes = model.Likes,
+                LocationNow = model.LocationNow,
+                MediumId = model.MediumId,
+                YearCreated = model.YearCreated,
+                ArtWorkAdderId = model.ArtWorkAdderId
+            };
+        }
     }
 }
