0ca8a454f18b2b588ea6b54c3b2e6bb27f88c14a
dannydjg16 <dannydjg16@gmail.com>
2022-07-10 22:44:58 -0400

    Location Domain location fields

--- MuseumApp.Domain/Models/Location.cs
+++ MuseumApp.Domain/Models/Location.cs
@@ -9,6 +9,10 @@ namespace MuseumApp.Domain.Models
         public string Description { get; set; }
         public string LocationUrl { get; set; }
         public int? TypeId { get; set; }
+        public string Country { get; set; }
+        public string StateProvince { get; set; }
+        public string City { get; set; }
+        public string StreetAddress { get; set; }
 
         public virtual LocationType Type { get; set; }
         public virtual IEnumerable<Artwork> Artworks { get; set; }
