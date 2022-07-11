b56af40463d7aeaf3d0f5517ba00cee870c1012a
dannydjg16 <dannydjg16@gmail.com>
2022-07-10 22:50:04 -0400

    Location Mapper with location fields

--- MuseumApp.DB/Mappers/LocationMapper.cs
+++ MuseumApp.DB/Mappers/LocationMapper.cs
@@ -28,5 +28,34 @@ namespace MuseumApp.DB.Mappers
                 Artworks = entity.Artworks.Select(ArtworkMapper.Map)
             };
         }
+
+        public static Location MapFull(Domain.Models.Location location)
+        {
+            return new Location
+            {
+                Id = location.Id,
+                LocationName = location.LocationName,
+                TypeId = location.TypeId,
+                LocationUrl = location.LocationUrl,
+                Description = location.Description,
+                Country = location.Country,
+                StateProvince = location.StateProvince,
+                City = location.City,
+                StreetAddress = location.StreetAddress
+            };
+        }
+
+        public static Domain.Models.Location MapFull(Location entity)
+        {
+            return new Domain.Models.Location
+            {
+                Id = entity.Id,
+                LocationName = entity.LocationName,
+                TypeId = entity.TypeId,
+                LocationUrl = entity.LocationUrl,
+                Description = entity.Description,
+                Artworks = entity.Artworks.Select(ArtworkMapper.Map)
+            };
+        }
     }
 }
