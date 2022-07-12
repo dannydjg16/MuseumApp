a31a47fd9be5f5ee119d964ae5f50ed784cff5d7
dannydjg16 <dannydjg16@gmail.com>
2022-07-12 15:51:51 -0400

    Passing Test for Artist Artwork

--- MuseumApp.Tests/Domain/ArtistTests.cs
+++ MuseumApp.Tests/Domain/ArtistTests.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using MuseumApp.Domain.Models;
+using System.Collections.Generic;
+
 namespace MuseumApp.Tests.Domain
 {
     public class ArtistTests
     {
-        public ArtistTests()
+        readonly Artist TestArtist = new Artist
         {
+            Artworks = new[] { new Artwork { Id = 1, Title = "Starry Night" } }
+        };
+
+        [Fact]
+        public void GetArtistsArtworkByID()
+        {
+            Assert.True(TestArtist.GetArtworkByID(1).Title == "Starry Night", "The chapters should have the same title");
         }
     }
 }
