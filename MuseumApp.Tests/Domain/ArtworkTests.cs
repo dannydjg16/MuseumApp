736173104dc26903cf3ac8cc8d78c632bf0ae28f
dannydjg16 <dannydjg16@gmail.com>
2022-08-09 09:14:43 -0400

    Art year not legitimate test

--- MuseumApp.Tests/Domain/ArtworkTests.cs
+++ MuseumApp.Tests/Domain/ArtworkTests.cs
@@ -10,7 +10,19 @@ namespace MuseumApp.Tests.Domain
             ArtistId = 1
         };
 
+        [Theory]
+        [InlineData(4000)]
+        [InlineData(2023)]
+        [InlineData(-60001)]
+        [InlineData(0)]
+        public void CheckArtYearIsNotLegitimate(int value)
+        {
+            TestArtwork.YearCreated = value;
 
+            bool realAge = TestArtwork.VerifyArtCreatedYear();
+
+            Assert.False(realAge);
+        }
 
     }
 }
