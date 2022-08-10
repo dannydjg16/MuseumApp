5bcf3f9ec598e6c73d8a25a4ca126d8acbbe3363
dannydjg16 <dannydjg16@gmail.com>
2022-08-10 10:09:34 -0400

    Artwork Model Tests

---
+++ MuseumApp.Tests/API/ArtworkModelTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MuseumApp.WebAPI.Models;
+
+namespace MuseumApp.Tests.API
+{
+    public class ArtworkModelTests
+    {
+        readonly ArtworkModel TestArtwork = new ArtworkModel
+        {
+            ArtistId = 1
+        };
+
+        [Theory]
+        [InlineData(4000)]
+        [InlineData(2023)]
+        [InlineData(-60001)]
+        [InlineData(0)]
+        public void CheckArtYearIsNotLegitimate(int value)
+        {
+            TestArtwork.YearCreated = value;
+
+            bool realAge = TestArtwork.VerifyArtCreatedYear();
+
+            Assert.False(realAge);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2022)]
+        [InlineData(-60000)]
+        public void CheckArtYearIsLegitimate(int value)
+        {
+            TestArtwork.YearCreated = value;
+
+            bool realAge = TestArtwork.VerifyArtCreatedYear();
+
+            Assert.True(realAge);
+        }
+    }
+}
+
