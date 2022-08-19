f0706574e6971946b6bd58dc77442bed58050da6
dannydjg16 <dannydjg16@gmail.com>
2022-08-19 12:49:44 -0400

    Adding Test Artworks to Location Test class

--- MuseumApp.Tests/Domain/LocationTests.cs
+++ MuseumApp.Tests/Domain/LocationTests.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using MuseumApp.Domain.Models;
+
 namespace MuseumApp.Tests.Domain
 {
     public class LocationTests
     {
-        public LocationTests()
+        readonly Artwork TestArtworkOne = new Artwork
         {
-        }
+            ArtistId = 1
+        };
+
+        readonly Artwork TestArtworkTwo = new Artwork
+        {
+            ArtistId = 2
+        };
+
+        readonly Artwork TestArtworkThree = new Artwork
+        {
+            ArtistId = 3
+        };
+
     }
 }
 
