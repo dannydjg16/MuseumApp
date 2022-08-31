018502ab76ce134598807cbf62e92cebcdba117a
dannydjg16 <dannydjg16@gmail.com>
2022-08-31 19:32:10 -0400

    User Full Mapper

--- MuseumApp.DB/Mappers/UserMapper.cs
+++ MuseumApp.DB/Mappers/UserMapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace MuseumApp.DB.Mappers
 {
@@ -34,6 +35,44 @@ namespace MuseumApp.DB.Mappers
                 CurrentCity = model.CurrentCity,
                 CurrentCountry = model.CurrentCountry,
                 CurrentStateProvince = model.CurrentStateProvince,
+            };
+        }
+
+        // Put in DB Model, Get back Domain Model
+        public static Domain.Models.User MapFull(User entity)
+        {
+            return new Domain.Models.User
+            {
+                ID = entity.Id,
+                Name = entity.Name,
+                Email = entity.Email,
+                FromLocation = entity.FromLocation,
+                ProfilePicURL = entity.ProfilePicUrl,
+                CurrentCity = entity.CurrentCity,
+                CurrentCountry = entity.CurrentCountry,
+                CurrentStateProvince = entity.CurrentStateProvince,
+                Likes = entity.Likes.Select(LikeMapper.Map),
+                Artists = entity.Artists.Select(ArtistMapper.Map),
+                Artworks = entity.Artworks.Select(ArtworkMapper.Map)
+            };
+        }
+
+        // Put in Domain Model, Get back DB Model
+        public static User MapFull(Domain.Models.User model)
+        {
+            return new User
+            {
+                Id = model.ID,
+                Name = model.Name,
+                Email = model.Email,
+                FromLocation = model.FromLocation,
+                ProfilePicUrl = model.ProfilePicURL,
+                CurrentCity = model.CurrentCity,
+                CurrentCountry = model.CurrentCountry,
+                CurrentStateProvince = model.CurrentStateProvince,
+                Likes = (ICollection<Like>)model.Likes.Select(LikeMapper.Map),
+                Artists = (ICollection<Artist>)model.Artists.Select(ArtistMapper.Map),
+                Artworks = (ICollection<Artwork>)model.Artworks.Select(ArtworkMapper.Map)
             };
         }
     }
