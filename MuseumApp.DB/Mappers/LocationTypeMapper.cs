d66a35ea3e4112eeeb6c1340faece015b2b55ec0
dannydjg16 <dannydjg16@gmail.com>
2022-08-31 19:32:05 -0400

    Location Type Full Mapper

--- MuseumApp.DB/Mappers/LocationTypeMapper.cs
+++ MuseumApp.DB/Mappers/LocationTypeMapper.cs
@@ -1,4 +1,8 @@
-﻿namespace MuseumApp.DB.Mappers
+﻿using System.Collections.Generic;
+using System.Linq;
+using MuseumApp.Domain.Models;
+
+namespace MuseumApp.DB.Mappers
 {
     public static class LocationTypeMapper
     {
@@ -19,6 +23,28 @@
             {
                 Id = entity.Id,
                 Name = entity.Name
+            };
+        }
+
+        // Put in Domain Model, Get back DB Model
+        public static LocationType MapFull(Domain.Models.LocationType locationType)
+        {
+            return new LocationType
+            {
+                Id = locationType.Id,
+                Name = locationType.Name,
+                Locations = (ICollection<Location>)locationType.Locations.Select(LocationMapper.Map)
+            };
+        }
+
+        // Put in DB Model, Get back Domain Model
+        public static Domain.Models.LocationType MapFull(LocationType entity)
+        {
+            return new Domain.Models.LocationType
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Locations = entity.Locations.Select(LocationMapper.Map)
             };
         }
     }
