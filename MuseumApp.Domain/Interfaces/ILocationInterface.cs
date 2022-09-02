af2f80a1661fc34f66b5d8a623c86c616b4ee46d
dannydjg16 <dannydjg16@gmail.com>
2022-09-02 18:48:15 -0400

    Location Interface Get FULL Methods

--- MuseumApp.Domain/Interfaces/ILocationInterface.cs
+++ MuseumApp.Domain/Interfaces/ILocationInterface.cs
@@ -8,7 +8,9 @@ namespace MuseumApp.Domain.Interfaces
         bool AddLocation(Location location);
         bool EditLocation(Location location);
         Location GetLocationById(int id);
+        Location GetFullLocationById(int id);
         IEnumerable<Location> GetLocations(string name = null);
+        IEnumerable<Location> GetFullLocations(string name = null);
         IEnumerable<Location> GetLocationsABC(string name = null);
     }
 }
